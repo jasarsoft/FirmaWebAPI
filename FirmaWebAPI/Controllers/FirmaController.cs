820ac9cae5abbeedb3ae33bed72478b0589fe082
Edin Jašarević <edinjasar14@gmail.com>
2018-09-27 16:41:09 +0200

    novi kontroler

---
+++ FirmaWebAPI/Controllers/FirmaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirmaWebAPI.Controllers
+{
+    public class FirmaController : Controller
+    {
+        public string Kvadriraj(int a)
+        {
+            return (a * a).ToString();
+        }
+    }
+}
