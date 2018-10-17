f7c7747c4617a84dd75d7b1d8f5933ec3359ce40
Edin Jašarević <edinjasar14@gmail.com>
2018-10-17 21:50:22 +0200

    metod obrisi

--- FirmaWebAPI/Controllers/FirmaController.cs
+++ FirmaWebAPI/Controllers/FirmaController.cs
@@ -3,6 +3,7 @@ using FirmaWebAPI.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -54,6 +55,17 @@ namespace FirmaWebAPI.Controllers
             }).ToList();
             
             return Json(firme, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Obrisi(int id)
+        {
+            firma2Entities db = new firma2Entities();
+            Firma f = db.Firma.Find(id);
+
+            db.Firma.Remove(f);
+            db.SaveChanges();
+
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
         }
     }
 }
